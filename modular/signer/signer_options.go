c39a6dc4718b54414e2f0f2c49c802a0787fc942
Alexgao001 <alex.g@nodereal.io>
2023-06-20 16:55:34 +0800

    fix: gas limit

--- modular/signer/signer_options.go
+++ modular/signer/signer_options.go
@@ -2,7 +2,6 @@ package signer
 
 import (
 	"fmt"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"os"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -55,13 +54,13 @@ func DefaultSignerOptions(signer *SignModular, cfg *gfspconfig.GfSpConfig) error
 		cfg.Chain.SealFeeAmount = DefaultSealFeeAmount
 	}
 	if cfg.Chain.RejectSealGasLimit == 0 {
-		cfg.Chain.SealGasLimit = DefaultRejectSealGasLimit
+		cfg.Chain.RejectSealGasLimit = DefaultRejectSealGasLimit
 	}
 	if cfg.Chain.RejectSealFeeAmount == 0 {
-		cfg.Chain.SealFeeAmount = DefaultRejectSealFeeAmount
+		cfg.Chain.RejectSealFeeAmount = DefaultRejectSealFeeAmount
 	}
 	if cfg.Chain.DiscontinueBucketGasLimit == 0 {
-		cfg.Chain.SealGasLimit = DefaultDiscontinueBucketGasLimit
+		cfg.Chain.DiscontinueBucketGasLimit = DefaultDiscontinueBucketGasLimit
 	}
 	if cfg.Chain.DiscontinueBucketFeeAmount == 0 {
 		cfg.Chain.DiscontinueBucketFeeAmount = DefaultDiscontinueBucketFeeAmount
